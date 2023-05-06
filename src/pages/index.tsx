eba766fbb0599452cc5300da4af052577dc0ffef
Ray Chung <raychungno1@gmail.com>
2023-05-06 17:12:56 -0400

    added posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,8 @@ import { UserButton, useUser, SignInButton } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
 
   const user = useUser();
   return (
@@ -18,6 +19,11 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{user.isSignedIn ? <UserButton /> : <SignInButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
