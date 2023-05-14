ea5b2ceccf12b9a05938d79c6ffe8aae8dd8b1c7
Ray Chung <raychungno1@gmail.com>
2023-05-14 11:16:05 -0400

    add ratelimiter to create empoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,6 +1,8 @@
 import { clerkClient } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 import { z } from "zod";
 
 import {
@@ -12,6 +14,13 @@ import {
 const filterUserForClient = ({ id, username, profileImageUrl }: User) => {
   return { id, username, profileImageUrl };
 };
+
+// Ratelimiter allowing 3 requests per minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+});
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -57,6 +66,10 @@ export const postsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+      
+      const { success } = await ratelimit.limit(authorId);
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
       const { content } = input;
 
       const post = await ctx.prisma.post.create({
