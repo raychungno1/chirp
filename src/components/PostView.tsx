f2d9c72d3cb754c60aa206b129db63cbeca463e6
Ray Chung <raychungno1@gmail.com>
2023-05-14 17:27:42 -0400

    updated post links

--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,36 +1,33 @@
 import dayjs from "dayjs";
 
 import { type RouterOutputs } from "~/utils/api";
-import Image from "next/image";
 import Link from "next/link";
+import ProfileImageLink from "./ProfileImageLink";
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-const PostView = ({ post, author }: PostWithUser) => {
+type PostViewProps = RouterOutputs["posts"]["getAll"][number] & {
+  link?: boolean;
+};
+const PostView = ({ post, author, link = false }: PostViewProps) => {
   const handle = `@${author.username}`;
 
-  return (
-    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
-        src={author.profileImageUrl}
-        alt={`${handle}'s profile image`}
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
+  const component = (
+    <div className="flex gap-3 border-b border-slate-400 p-4">
+      <ProfileImageLink {...author} />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <Link href={`/${handle}`}>
-            <span>{handle}</span>
+          <Link href={`/${handle}`} className="font-bold hover:underline">
+            {handle}
           </Link>
-          <Link href={`/post/${post.id}`} className="flex gap-1">
-            <span>·</span>
-            <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
-          </Link>
+          <span>·</span>
+          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
+
+  if (link) return <Link href={`/post/${post.id}`}>{component}</Link>;
+  return component;
 };
 
 export default PostView;
